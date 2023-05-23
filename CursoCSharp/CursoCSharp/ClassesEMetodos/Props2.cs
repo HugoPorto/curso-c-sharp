97a31bcfeb37f0978c73cb25e11375929fcbc3a4
Hugo Porto <45318310+HugoPorto@users.noreply.github.com>
2023-05-22 21:39:58 -0300

    Update Props2.cs

--- CursoCSharp/CursoCSharp/ClassesEMetodos/Props2.cs
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/Props2.cs
@@ -1,61 +1,64 @@
 using System;
 
-public class CarroOpcional
+namespace CursoCSharp.ClassesEMetodos
 {
-    double desconto = 0.1;
-
-    string nome;
-    public string Nome
+    public class CarroOpcional
     {
-        get { return "Opcional: " + nome; }
-        set { nome = value; }
-    }
+        double desconto = 0.1;
 
-    private double preco;
-    public double Preco
-    {
-        get { return preco; }
-        set
+        string nome;
+        public string Nome
         {
-            if (value >= 0)
-                preco = value;
-            else
-                throw new ArgumentException("O preço deve ser um valor positivo.");
+            get { return "Opcional: " + nome; }
+            set { nome = value; }
+        }
+
+        private double preco;
+        public double Preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value >= 0)
+                    preco = value;
+                else
+                    throw new ArgumentException("O preço deve ser um valor positivo.");
+            }
+        }
+
+        public double PrecoComDesconto => Preco - (desconto * Preco);
+
+        public CarroOpcional()
+        {
+
+        }
+
+        public CarroOpcional(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
         }
     }
 
-    public double PrecoComDesconto => Preco - (desconto * Preco);
-
-    public CarroOpcional()
+    class Props
     {
+        public static void Executar()
+        {
+            var op1 = new CarroOpcional("Ar Condicionado", 3499.9);
+            Console.WriteLine(op1.PrecoComDesconto);
 
-    }
+            // op1.PrecoComDesconto = 3000;
 
-    public CarroOpcional(string nome, double preco)
-    {
-        Nome = nome;
-        Preco = preco;
-    }
-}
+            var op2 = new CarroOpcional();
+            op2.Nome = "Direção Elétrica";
+            op2.Preco = 2349.9;
 
-class Props
-{
-    public static void Executar()
-    {
-        var op1 = new CarroOpcional("Ar Condicionado", 3499.9);
-        Console.WriteLine(op1.PrecoComDesconto);
+            Console.WriteLine(op1.Nome);
+            Console.WriteLine(op1.Preco); // Exibe o preço do opcional 'op1'
 
-        // op1.PrecoComDesconto = 3000;
-
-        var op2 = new CarroOpcional();
-        op2.Nome = "Direção Elétrica";
-        op2.Preco = 2349.9;
-
-        Console.WriteLine(op1.Nome);
-        Console.WriteLine(op1.Preco); // Exibe o preço do opcional 'op1'
-
-        Console.WriteLine(op2.Nome);
-        Console.WriteLine(op2.Preco); // Exibe o preço do opcional 'op2'
-        Console.WriteLine(op2.PrecoComDesconto);
+            Console.WriteLine(op2.Nome);
+            Console.WriteLine(op2.Preco); // Exibe o preço do opcional 'op2'
+            Console.WriteLine(op2.PrecoComDesconto);
+        }
     }
 }
